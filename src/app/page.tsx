e23257f4064afd3c20054126db0ab98c59925898
Drew Wyatt <drew.j.wyatt@gmail.com>
2023-07-30 18:11:23 -0400

    update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Connect Four</h1>
+      <h1 className={styles.headline}>Connect Four</h1>
       <div className={styles.center}>
         <Board />
       </div>
