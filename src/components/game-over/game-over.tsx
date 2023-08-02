e3d0c6674ce278f6edeadc3711d1e6c6c615cfb6
Drew Wyatt <drew.wyatt@classdojo.com>
2023-08-01 20:57:38 -0400

    add game over modal

--- src/components/game-over/game-over.tsx
+++ src/components/game-over/game-over.tsx
@@ -1,21 +1,38 @@
 'use client'
-import type { FC } from 'react'
+import { FC, useCallback, useEffect, useRef } from 'react'
 import { Token, playerNameForToken } from '~/lib/models'
 import { useGameOver, useReset } from '~/lib/state'
+import styles from './game-over.module.css'
 
-const Heading: FC<{ winner: Token | null }> = ({ winner }) =>
-  !!winner ? <h1>{playerNameForToken(winner)} Wins!</h1> : <h1>It's a Draw!</h1>
+const Heading: FC<{ winner: Token | null }> = ({ winner }) => (
+  <h1 className={styles.heading}>
+    {!!winner ? `${playerNameForToken(winner)} Wins!` : "It's a Draw!"}
+  </h1>
+)
 
 const GameOver: FC = () => {
   const [isGameOver, winner] = useGameOver()
   const reset = useReset()
+  const self = useRef<HTMLDialogElement | null>(null)
+
+  const onClick = useCallback(() => {
+    reset()
+    self.current?.close()
+  }, [reset])
+
+  useEffect(() => {
+    if (isGameOver) {
+      self.current?.showModal()
+    }
+  }, [isGameOver])
+
   return (
-    isGameOver && (
-      <dialog open>
-        <Heading winner={winner} />
-        <button onClick={reset}>Play Again</button>
-      </dialog>
-    )
+    <dialog ref={self} className={styles.gameOver}>
+      <Heading winner={winner} />
+      <button className={styles.playAgain} onClick={onClick}>
+        Play Again
+      </button>
+    </dialog>
   )
 }
 
